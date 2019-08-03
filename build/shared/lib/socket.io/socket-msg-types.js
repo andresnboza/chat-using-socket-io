8660d04a2145fe49dfc196525653e2f2c542594d
andresnboza <andresnboza92@gmail.com>
2019-08-03 00:39:08 -0600

    gitignore now correct

--- build/shared/lib/socket.io/socket-msg-types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SocketMessageType;
-(function (SocketMessageType) {
-    SocketMessageType["ASSIGN_CLIENT_ID"] = "assignClientId";
-    SocketMessageType["UPDATE_USERS"] = "updateUsers";
-    SocketMessageType["UPDATE_USER_GROUPS"] = "updateUserGroups";
-    SocketMessageType["UPDATE_ROLES"] = "updateRoles";
-})(SocketMessageType = exports.SocketMessageType || (exports.SocketMessageType = {}));
